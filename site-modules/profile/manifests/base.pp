cd5b246665262fdc88c8ccd112d4f523ab013d7b
Jake Spain <jakespain1@gmail.com>
2021-08-28 18:32:31 -0400

    Added nftables service resource masked

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,5 +1,10 @@
 class profile::base {
 
   #the base profile should include component modules that will be on all nodes
+  if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] >= '8' {
+    service { 'nftables':
+      enable => 'mask',
+    }
+  }
 
 }
